99cb6de4d1cdef92ae35350a0c09e2de27965a37
RomoL2 <106694796+RomoL2@users.noreply.github.com>
2023-07-25 19:29:04 -0400

    fixed bug

--- R/install_reqs.R
+++ R/install_reqs.R
@@ -11,15 +11,16 @@ install_reqs <- function(path_to_package) {
   project_dir<-list.files(pattern = "fai$", recursive = TRUE)[1]
   project_dir<-stringr::str_extract(project_dir[1], '.*extdata.')
   setwd(project_dir)
-  #check if conda/python/PIP installed
   #install hg38 fasta
   system('wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz')
   #set up conda environment RBPamp
-  reticulate::conda_create(envname ='RBPamp')
+  setwd('./RBPamp')
+  reticulate::conda_create(envname ='RBPamp', environment = 'environment.yml')
   reticulate::conda_install('RBPamp', 'cython')
   path_to_python<-reticulate::conda_list()[[2]][which(reticulate::conda_list()[[1]]=='RBPamp')]
   reticulate::use_python(path_to_python, required = TRUE)
   reticulate::use_condaenv('RBPamp', required=TRUE)
+  system('export CC=gcc')
   system('python setup.py build')
   system('pip install .')
 }
