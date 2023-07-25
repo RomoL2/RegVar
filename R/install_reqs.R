80553c4cd7bff3350ee19a329fee515800cccac4
RomoL2 <106694796+RomoL2@users.noreply.github.com>
2023-07-25 13:49:09 -0400

    fixed conda env
    fixed RBPamp conda env

--- R/install_reqs.R
+++ R/install_reqs.R
@@ -15,7 +15,7 @@ install_reqs <- function(path_to_package) {
   #install hg38 fasta
   system('wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz')
   #set up conda environment RBPamp
-  reticulate::conda_create(envname ='RBPamp', environment = 'environment.yml')
+  reticulate::conda_create(envname ='RBPamp')
   reticulate::conda_install('RBPamp', 'cython')
   path_to_python<-reticulate::conda_list()[[2]][which(reticulate::conda_list()[[1]]=='RBPamp')]
   reticulate::use_python(path_to_python, required = TRUE)
